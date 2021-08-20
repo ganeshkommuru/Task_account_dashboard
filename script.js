fcf473d97f8bd662d2aa91b9aee8addc47e3ce76
ganeshkommuru <77089145+ganeshkommuru@users.noreply.github.com>
2021-08-20 17:30:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 function myFunction() {
     var x = document.getElementById("h_b_links_id");
+    console.log(x)
     if (x.className === "h_b_links") {
-      x.className += " responsive";
+      x.className = "responsive";
     } else {
       x.className = "h_b_links";
     }
