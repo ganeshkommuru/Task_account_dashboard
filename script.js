387a84c6b386d3658d4055e0ec513718931706ba
ganeshkommuru <77089145+ganeshkommuru@users.noreply.github.com>
2021-08-25 16:26:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 function myFunction() {
-    var x = document.getElementById("h_b_links_id");
-    console.log(x)
-    if (x.className === "h_b_links") {
-      x.className = "responsive";
-    } else {
-      x.className = "h_b_links";
-    }
+  let x = document.getElementById("h_b_links_id");
+  if (x.className === "h_b_links") {
+    x.className = "responsive";
+  } else {
+    x.className = "h_b_links";
   }
+  let y = document.getElementById('toggle_icon');
+  y.classList.toggle('fa-bars');
+  y.classList.toggle('fa-times');
+}
+
+function openForm() {
+  document.getElementById("form_popup").style.display = "block";
+}
+  
+function closeForm() {
+  document.getElementById("form_popup").style.display = "none";
+}
